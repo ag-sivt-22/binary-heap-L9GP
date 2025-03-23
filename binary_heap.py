8566a0824a16aed8be0392b79cc845c54bcb0185
L9GP <klimas@arcig.cz>
2025-03-23 14:30:50 +0100

    prosim fungovat

--- binary_heap.py
+++ binary_heap.py
@@ -14,28 +14,52 @@ class BinaryHeap:
     
     def push(self, element):
         # naivni implementace binarni haldy
+        self.proprint = element
         self.heap.append(element)
+        self.push_reorder(len(self.heap) - 1)
+    
+    def push_reorder(self, index):
+        new_elmnt_index = index
+        parent_index = (new_elmnt_index - 1) // 2
+
+        if self.heap[new_elmnt_index].priority < self.heap[parent_index].priority and parent_index >= 0:
+            print(parent_index)
+            print(self.proprint)
+            self.heap[parent_index], self.heap[new_elmnt_index] = self.heap[new_elmnt_index], self.heap[parent_index]
+            self.push_reorder(parent_index)
 
     def pop(self):
         # naivni implementace binarni haldy
         # najdi nejmensi prvek a vrat ho
         if not self.heap:
             raise Exception("Heap is empty")
-        min_idx = 0
-        for idx in range(1,len(self.heap)):
-            if self.heap[idx].priority < self.heap[min_idx].priority:
-                min_idx = idx
-        return self.heap.pop(min_idx)
+        # Nevim jestli tomu rozumím správně, ale pokud bude halda vždy správně seřazená, tak kořen bude vždy mít "nejmenší" prioritu
+        self.return_value = self.heap[0]
+        self.heap.pop(0)
+        self.pop_reorder()
+    
+    def pop_reorder(self):
+        for i in self.heap:
+            self.push_reorder(self.heap.index(i))
 
+        return self.return_value
 
     def head(self):
         if not self.heap:
             raise Exception("Heap is empty")
         # vrati nejmensi element ve fronte (element na cele fronty)
         # protoze mame naivni implementaci, musime projit cely seznam
-        min_idx = 0
-        for idx in range(1,len(self.heap)):
-            if self.heap[idx].priority < self.heap[min_idx].priority:
-                min_idx = idx
-        return self.heap[min_idx]
-        
+        return self.heap[0]
+
+"""el1 = Element(12, 8)
+el2 = Element(12, 4)
+el3 = Element(12, 5)
+el4 = Element(12, 1)
+
+bh = BinaryHeap()
+bh.push(el1)
+bh.push(el2)
+bh.push(el3)
+bh.push(el4)
+bh.pop()
+print(bh.heap)"""
